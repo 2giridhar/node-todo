0403cfa735121e457111bc63db3d06774eb8001b
Giridhar Peddabuttaiahgari <2giridhar.reddy@gmail.com>
2016-03-27 02:10:19 -0700

    API Done commit
    API complete

--- app/routes.js
+++ app/routes.js
@@ -1,58 +1,80 @@
-var Todo = require('./models/todo');
+var Food = require('./models/food');
+var path = require("path");
 
-function getTodos(res) {
-    Todo.find(function (err, todos) {
-
+function getFoods(res) {
+    Food.find(function (err, food) {
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err) {
             res.send(err);
         }
 
-        res.json(todos); // return all todos in JSON format
+        res.json(food); // return all foods in JSON format
     });
 }
-;
 
+function getTotal(res){
+    Food.aggregate({
+        $group:{
+            "_id": null,
+            "total":{$sum: "$price"}
+        }
+    },function (err, total) {
+        if (err)
+            res.send(err);
+
+        if(total[0] == undefined){
+            res.json(0);
+        }else{
+            res.json(total[0].total);
+        }
+
+    });
+}
 module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
-    // get all todos
-    app.get('/api/todos', function (req, res) {
-        // use mongoose to get all todos in the database
-        getTodos(res);
+    // get all food items
+    app.get('/api/food', function (req, res) {
+        // use mongoose to get all food items in the database
+        getFoods(res);
     });
 
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function (req, res) {
+    // create food and send back all food items after creation
+    app.post('/api/food', function (req, res) {
 
-        // create a todo, information comes from AJAX request from Angular
-        Todo.create({
-            text: req.body.text,
-            done: false
-        }, function (err, todo) {
+        // create a food item, information comes from AJAX request from Angular
+        Food.create({
+            name: req.body.name,
+            price: req.body.price
+        }, function (err, food) {
             if (err)
                 res.send(err);
 
-            // get and return all the todos after you create another
-            getTodos(res);
+            // get and return all the food items after you create another
+            getFoods(res);
         });
 
     });
 
-    // delete a todo
-    app.delete('/api/todos/:todo_id', function (req, res) {
-        Todo.remove({
-            _id: req.params.todo_id
-        }, function (err, todo) {
+    // delete a food
+    app.delete('/api/food/:food_id', function (req, res) {
+        Food.remove({
+            _id: req.params.food_id
+        }, function (err, food) {
             if (err)
                 res.send(err);
 
-            getTodos(res);
+            getFoods(res);
         });
+    });
+
+    // get total price of food items
+    app.get('/api/total', function (req, res){
+        getTotal(res);
     });
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(path.join(__dirname,'../', '/public/index.html')); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
